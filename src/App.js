2696d7de1e7e2f81662a20121613ce23a8b78589
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-19 11:13:37 +0300

    5.3 blogilistan frontend, osa 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Blog from './components/Blog'
+import MessageNotification from './components/MessageNotification'
+import ErrorNotification from './components/ErrorNotification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       blogs: [],
+      blogTitle: '',
+      blogAuthor: '',
+      blogUrl: '',
       username: '',
       password: '',
+      message: null,
       error: null,
       user: null
     }
@@ -37,10 +44,12 @@ class App extends React.Component {
       })
 
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
-      this.setState({ username: '', password: '', user})
+      blogService.setToken(user.token)
+
+        this.setState({ username: '', password: '', user })
     } catch(exception) {
         this.setState({
-          error: 'käyttäjätunnus tai salasana virheellinen',
+          error: 'wrong username or password',
         })
         setTimeout(() => {
           this.setState({ error: null })
@@ -51,18 +60,95 @@ class App extends React.Component {
   logout = async (event) => {
     event.preventDefault()
     window.localStorage.removeItem('loggedBlogappUser')
-    this.setState({user: null})
+    this.setState({ user: null})
+  }
+
+  addBlog = (event) => {
+    event.preventDefault()
+
+      const blogObj = {
+        title: this.state.blogTitle,
+        author: this.state.blogAuthor,
+        url: this.state.blogUrl
+      }
+      blogService
+        .create(blogObj)
+        .then(newBlog => {
+          this.setState({
+              blogs: this.state.blogs.concat(newBlog),
+              message: `a new blog '${newBlog.title}' by ${newBlog.author} added`,
+              blogTitle: '',
+              blogAuthor: '',
+              blogUrl: ''
+            })
+            setTimeout(() => {
+              this.setState({ message: null })
+            }, 5000)
+        })
+        .catch(error => {
+          this.setState({
+            error: 'something went wrong',
+            blogTitle: '',
+            blogAuthor: '',
+            blogUrl: ''
+          })
+          setTimeout(() => {
+            this.setState({ error: null })
+          }, 5000)
+        })
   }
 
   handleLoginFieldChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
 
+  handleBlogFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
   render() {
+    const blogForm = () => (
+      <div>
+        <h2>create new</h2>
+
+        <form onSubmit={this.addBlog}>
+          <div>
+            Title
+            <input
+              type="text"
+              name="blogTitle"
+              value={this.state.blogTitle}
+              onChange={this.handleBlogFieldChange}
+            />
+          </div>
+          <div>
+            Author
+            <input
+              type="text"
+              name="blogAuthor"
+              value={this.state.blogAuthor}
+              onChange={this.handleBlogFieldChange}
+            />
+          </div>
+          <div>
+            Url
+            <input
+              type="text"
+              name="blogUrl"
+              value={this.state.blogUrl}
+              onChange={this.handleBlogFieldChange}
+            />
+          </div>
+          <button type="submit">tallenna</button>
+        </form>
+      </div>
+    )
 
     if (this.state.user === null) {
+      console.log('ERROR::::  ', this.state.error)
       return (
         <div>
+        <ErrorNotification error={this.state.error} />
         <h2>Kirjaudu sovellukseen</h2>
         <form onSubmit={this.login}>
           <div>
@@ -91,11 +177,19 @@ class App extends React.Component {
 
     return (
       <div>
+
+      <MessageNotification message={this.state.message} />
+      <ErrorNotification error={this.state.error} />
+
         <h2>blogs</h2>
+
         <p>
-        {window.localStorage.getItem('nimi')} loggedin
-        <button onClick={this.logout}>logout</button>
+          {this.state.user.name} logged in
+          <button onClick={this.logout}>logout</button>
         </p>
+
+        {blogForm()}
+
         {this.state.blogs.map(blog =>
           <Blog key={blog.id} blog={blog}/>
         )}
