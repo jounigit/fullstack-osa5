fe139af401a5699ddb6962e58537ce19147f98fb
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-07-11 00:30:00 +0300

    5.16 testi ei toimi

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import App from './App'
+import localStorage from './setupTests'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 jest.mock('./services/blogs')
@@ -23,6 +24,44 @@ describe('<App />', () => {
       app.update()
       const loginFormComponents = app.find(LoginForm)
       expect(loginFormComponents.length).toBe(1)
+    })
+  })
+
+  describe('when user is logged', () => {
+    beforeEach(() => {
+      app = mount(<App />)
+      const user = {
+        username: 'tester',
+        token: '1231231214',
+        name: 'Teuvo Testaaja'
+      }
+      localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+    })
+
+    /*let savedItems = {}
+
+    const localStorageMock = {
+      setItem: (key, item) => {
+        savedItem[key] = item
+      },
+      getItem: (key) => savedItems[key],
+      clear: savedItems = {}
+    }*/
+    /*
+    const user = {
+      username: 'tester',
+      token: '1231231214',
+      name: 'Teuvo Testaaja'
+    }
+    */
+    //const localStorage = localStorageMock.setItem('loggedBlogAppUser', JSON.stringify(user))
+
+  //  window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+
+    it('all blogs are rendered', () => {
+      app.update()
+      const blogComponents = app.find(Blog)
+    expect(blogComponents.length).toEqual(blogService.blogs.length)
     })
   })
 })
