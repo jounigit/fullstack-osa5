a0623a07663e995a0e6f2a221211490343a1f907
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-07-16 21:32:44 +0300

    5.19 anekdootit, osa 1

--- src/index.js
+++ src/index.js
@@ -1,97 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
-import counterReducer from './reducer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import App from './App';
+import reducer from './reducer'
 
-const store = createStore(counterReducer)
-console.log(store.getState())
+const store = createStore(reducer)
 
-
-const Statistiikka = ({onClick}) => {
-  const palautteita = store.getState()
-  const keskiarvo = palautteita.good - palautteita.bad / 2
-  const countPos = (palautteita.good * 100) / (palautteita.good + palautteita.ok + palautteita.bad)
-  const positiivisia = palautteita.good === 0 ? 0 : countPos.toFixed(1)
-
-  if (palautteita === 0) {
-    return (
-      <div>
-        <h2>statistiikka</h2>
-        <div>ei yhtään palautetta annettu</div>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>statistiikka</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>hyvä</td>
-            <td>{palautteita.good}</td>
-          </tr>
-          <tr>
-            <td>neutraali</td>
-            <td>{palautteita.ok}</td>
-          </tr>
-          <tr>
-            <td>huono</td>
-            <td>{palautteita.bad}</td>
-          </tr>
-          <tr>
-            <td>keskiarvo</td>
-            <td>{keskiarvo}</td>
-          </tr>
-          <tr>
-            <td>positiivisia</td>
-            <td>{positiivisia} %</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <button onClick={onClick}>nollaa tilasto</button>
-    </div >
+const render = () => {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
   )
 }
 
-class App extends React.Component {
-  klik = (nappi) => () => {
-    switch (nappi) {
-      case 'GOOD':
-        store.dispatch({ type: 'GOOD' })
-        break
-      case 'OK':
-        store.dispatch({ type: 'OK' })
-        break
-      case 'BAD':
-        store.dispatch({ type: 'BAD' })
-        break
-      case 'ZERO':
-        store.dispatch({ type: 'ZERO' })
-        break
-      default:
-        store.dispatch({ type: 'DO_NOTHING' })
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>anna palautetta</h2>
-        <button onClick={this.klik('GOOD')}>hyvä</button>
-        <button onClick={this.klik('OK')}>neutraali</button>
-        <button onClick={this.klik('BAD')}>huono</button>
-        <Statistiikka onClick={this.klik('ZERO')} />
-        {console.log(store.getState())}
-      </div>
-    )
-  }
-}
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
+render()
+store.subscribe(render)
--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,40 @@
-const initialState = {
-  good: 0,
-  ok: 0,
-  bad: 0
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000*Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
 }
 
-const counterReducer = (state = initialState, action) => {
-  console.log(action)
-  switch (action.type) {
-    case 'GOOD':
-      let newState = {...state, good: state.good + 1}
-      console.log('NEWSTATE:: ', newState)
-      return {...state, ...newState}
-    case 'OK':
-      return {...state, ok: state.ok + 1}
-    case 'BAD':
-      return {...state, bad: state.bad + 1}
-    case 'ZERO':
-      return state = initialState
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ',state)
+  console.log('action', action)
+
+  switch(action.type) {
+    case 'TOGGLE_VOTE':
+      console.log('toggle vote:', action.data.id)
+      const id = action.data.id
+      const anecdoteToChange = state.find(n => n.id === id)
+      console.log('toggled anec:', anecdoteToChange)
+      const changed = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1 }
+      console.log('toggled changed:', changed)
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changed )
     default:
       return state
   }
 }
 
-export default counterReducer
+export default reducer
